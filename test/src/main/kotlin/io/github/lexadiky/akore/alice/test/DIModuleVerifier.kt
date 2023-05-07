3da0bb7f47e11b0269bf8e9bef3a335c3ca6c215
Aleksey Yakovlev <alexey.yakovlef@gmail.com>
2023-05-08 03:05:15 +0700

    - api fix

--- test/src/main/kotlin/io/github/lexadiky/akore/alice/test/DIModuleVerifier.kt
+++ test/src/main/kotlin/io/github/lexadiky/akore/alice/test/DIModuleVerifier.kt
@@ -4,9 +4,18 @@ import io.github.lexadiky.akore.alice.AliceInternalApi
 import io.github.lexadiky.akore.alice.DIModule
 import kotlin.reflect.KClass
 import org.koin.core.annotation.KoinExperimentalAPI
+import org.koin.test.verify.MissingKoinDefinitionException
 import org.koin.test.verify.verify
 
 @OptIn(AliceInternalApi::class, KoinExperimentalAPI::class)
-fun DIModule.isFullyDefined(extraTypes: List<KClass<*>> = emptyList()) = koinModule.verify(
-    extraTypes = extraTypes
-)
+fun DIModule.isFullyDefined(extraTypes: List<KClass<*>> = emptyList(), rethrow: Boolean = false): Boolean = try {
+    koinModule.verify(
+        extraTypes = extraTypes
+    )
+    true
+} catch (e: MissingKoinDefinitionException) {
+    if (rethrow) {
+        throw e
+    }
+    false
+}
